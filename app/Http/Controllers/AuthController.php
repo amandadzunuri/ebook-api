3b9acaa4e85fc972ad3838b8a805b99633c455bb
amandadzunuri <amandadzunuri@gmail.com>
2020-08-16 20:59:42 +0700

    ketiga

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    function me(){
+        return "NIS : 3103118016
+                Nama : Amanda Dzunuri Elvira Yaniar
+                Gender  : Perempuan
+                Kelas : XII RPL 1";
+    }
+}
